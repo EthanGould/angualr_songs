a8e0b135b7a414575483a6b03d5a12c96ef68596
Ethan Gould <ethan.g.907@gmail.com>
2014-06-20 11:25:20 -0400

    created files for songs

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,17 @@
+class SongsController < ApplicationController
+  respond_to :json
+
+  def index
+    @songs = Song.all
+    respond_with(@songs) do |format|
+      format.json { render :json => @songs, :root => false }
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    respond_with(@song) do |format|
+      format.json { render :json => @song}
+    end
+  end
+end
