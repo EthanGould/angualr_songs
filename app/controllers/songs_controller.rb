e187b71d251abe03ef0e2443e46006ec084fd37e
Ethan Gould <ethan.g.907@gmail.com>
2014-06-20 16:27:28 -0400

    add index method to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,12 @@
 class SongsController < ApplicationController
 
+  respond_to :json
+
   def index
+    @songs = Song.all
+    respond_with(@songs) do |format|
+      format.json { render :json => @songs, :root => false }
+    end
   end
+
 end
