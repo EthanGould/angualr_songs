7769506945f05c82dae0ef2ff9a46980e91664fb
Ethan Gould <ethan.g.907@gmail.com>
2014-06-20 14:45:44 -0400

    updated routes for new 'main' controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
+  get 'main/index'
+  root to: 'main#index'
   resources :songs
 end
