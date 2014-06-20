79a85bffba4acd22926427fc37f75734221f8265
Ethan Gould <ethan.g.907@gmail.com>
2014-06-20 11:24:21 -0400

    added seed data for songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.delete_all
+
+20.times do |i|
+  Song.create(title: "Song #{i}", artist: "This is song #{i}", price: (rand(1).to_f + (rand(100)/100.00)) )
+end
